dd9b313b63cd69e01b56f9dfff357920a9fe36f9
Franklin <franklin.op@hotmail.com>
2024-04-30 12:02:56 +0200

    Mongo connection and mongoose working, installed colors for terminal settings

--- config/db.js
+++ config/db.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    const conn = await mongoose.connect();
-}
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+    } catch (err) {
+        console.error(`Error: ${err.message}`);
+        process.exit(1); // Exit process with failure
+    }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require ('express');
 const dotenv = require ('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db')
+
+
+//Load env vars 
+dotenv.config({path: './config/config.env'});
+
+//Connect to database
+connectDB();
 
 
 //Route files 
 const bootcamps = require ('./routes/bootcamps');
 
-
-//Load env vars 
-dotenv.config({path: './config/config.env'});
 
 const app = express();
 
@@ -25,6 +31,13 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5300;
 
-app.listen(
+const server = app.listen(
     PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+
+ //Handle unhandled promise rejections  
+ process.on('unhandledRejection', (err,promise) => {
+   console.log(`Error: ${err.message}`.red);
+   //Close server & exit process
+   server.close(()=> process.exit(1));
+ })
