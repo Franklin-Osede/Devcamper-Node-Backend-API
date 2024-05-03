10aab517d3259388496da4fed70e8f958ba122d6
Franklin <franklin.op@hotmail.com>
2024-05-03 12:54:26 +0200

    updated error on hadnler

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -32,7 +32,7 @@ exports.getBootcamp = async(req, res, next) => {
     res.status(200).json({success: true, data: bootcamp});
     
    } catch (error) {
-    next(err);
+    next(error);
    }
 }
 
--- middleware/error.js
+++ middleware/error.js
@@ -15,7 +15,7 @@ const errorHandler = (err, req, res, next)=> {
     }
 
 
-    res.status( err.statusCode || 500).json({
+    res.status( error.statusCode || 500).json({
         success: false,
         error: err.message || 'Server Error'
     });
