71da842ff18ba66469208158499e08aa7dbf94e4
Franklin <franklin.op@hotmail.com>
2024-05-24 17:33:19 +0200

    created modal and seeder

--- controllers/courses.js
+++ controllers/courses.js
@@ -9,5 +9,19 @@ const Course = require('../models/Course');
 //@access  Public
 
 exports.getCourses = asyncHandler(async(req, res, next)=>{
-    
+    let query;
+
+    if(req.params.bootcampId){
+        query = Course.find({bootcamp: req.params.bootcampId});
+    }else {
+        query = Course.find();
+    }
+
+    const courses = await query;
+
+    res.status (200).json({
+        success: true,
+        count: courses.length,
+        data: courses
+    })
 })
